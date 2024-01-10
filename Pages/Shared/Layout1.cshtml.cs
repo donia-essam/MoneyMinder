016b00eb2c7459fff5948e19c3f54ad5d01b1329
donia-essam <s-donia.soliman@zewailcity.edu.eg>
2024-01-10 20:59:58 +0200

    Add project files.

---
+++ Pages/Shared/Layout1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MoneyMinder.Pages.Shared
+{
+    public class Layout1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
